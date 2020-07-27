df04bf48e33843ba2847f771b06f190403bd5e62
Pedro Henrique <pedro.aguiar@camanor.com.br>
2020-07-26 23:18:05 -0300

    Implementação do layout e funcionalidades de busca dos devs

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Profile()
+function Profile({ navigation })
 {
-    return <View />
+    const gitHubUser = navigation.getParam('github_user');
+
+    return <WebView source={{ uri: `https://github.com/${gitHubUser}` }} style={{ flex: 1 }} />
 }
 
 export default Profile;
